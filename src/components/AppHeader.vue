8cc36174a1db9b6b90252e9a540009d504500ef7
RobertoZefi <zefi.roberto2001@gmail.com>
2023-03-01 16:39:09 +0100

    FEAT header added

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -12,11 +12,22 @@ export default{
 <div class="container">
     <div class="row">
         <div class="col">
-            <img src="" alt="">
+            <img src="/images/dc-logo.png" alt="">
         </div>
 
         <div class="col">
-
+            <ul class="list">
+                <li>characters</li>
+                <li>comics</li>
+                <li>movies</li>
+                <li>tv</li>
+                <li>games</li>
+                <li>collectibles</li>
+                <li>videos</li>
+                <li>fans</li>
+                <li>news</li>
+                <li>shop</li>
+            </ul>
         </div>
     </div>
 </div>
@@ -24,6 +35,22 @@ export default{
 </template>
 
 <style scoped lang="scss">
-@use '../partials/style/variables.scss'
+@use '../partials/style/variables.scss';
 
+.container{
+    padding: 20px 0;
+}
+.row{
+    justify-content: space-between;
+    align-items: center;
+}
+.list{
+    display: flex;
+    gap: 33px;
+
+    li{
+        text-transform: uppercase;
+        font-size: 15px;
+    }
+}
 </style>
