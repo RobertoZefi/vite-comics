6dbd48cba4e7b5a4ba07b85c7fd5d8bd59279e12
RobertoZefi <zefi.roberto2001@gmail.com>
2023-03-01 16:16:03 +0100

    FEAT start header

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,29 @@
+<script>
+export default{
+    data(){
+        return{
+            
+        }
+    }
+}
+</script>
+
+<template>  
+<div class="container">
+    <div class="row">
+        <div class="col">
+            <img src="" alt="">
+        </div>
+
+        <div class="col">
+
+        </div>
+    </div>
+</div>
+
+</template>
+
+<style scoped lang="scss">
+@use '../partials/style/variables.scss'
+
+</style>
