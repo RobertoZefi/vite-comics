361a599e50044d73cb657e93728ba8358776de9a
RobertoZefi <zefi.roberto2001@gmail.com>
2023-03-02 17:06:38 +0100

    FEAT start hero

---
+++ src/components/Card.vue
@@ -0,0 +1,36 @@
+<script>
+export default{
+props:{
+    image: {
+        type: String,
+        reaquired: true,
+    },
+    text: {
+        type: String,
+        reaquired: true,
+    }
+}
+}
+</script>
+
+<template>
+<div class="card">
+    <img :src="image" alt="">
+    <h4 class="title">{{ text }}</h4>
+</div>
+</template>
+
+<style lang="scss">
+
+.card{
+    img{
+        width: 185px;
+        aspect-ratio: 1;
+    }
+    .title{
+        text-transform: uppercase;
+        color: white;
+        font-weight: 500;
+    }
+}
+</style>
